ee3098e59b88a535fe5b32caeac802e084d4962e
Gabriel J. Ferrer <ferrer@hendrix.edu>
2023-04-07 17:04:03 -0500

    More progress on interpreter.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(test), no_std)]
 
-/* 
+/*
 const GRAMMAR: &str = r#"
     script     := line script | line ;
     line       := statement '\n' ;
@@ -27,68 +27,148 @@ const GRAMMAR: &str = r#"
 "#;
 */
 
+use core::cmp::min;
+
 use bare_metal_map::BareMetalMap;
-use gc_heap::{Pointer, CopyingHeap, Tracer};
+use gc_heap::{CopyingHeap, Pointer, Tracer, HeapResult};
 
 pub trait InterpreterIo {
     fn print(&mut self, chars: &[u8]);
     fn input(&mut self, buffer: &mut [u8]);
 }
 
-pub struct Interpreter<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize, const STACK_DEPTH: usize, const MAX_LOCAL_VARS: usize, const HEAP_SIZE: usize, const MAX_BLOCKS: usize> {
+pub struct Interpreter<
+    const MAX_TOKENS: usize,
+    const MAX_LITERAL_CHARS: usize,
+    const STACK_DEPTH: usize,
+    const MAX_LOCAL_VARS: usize,
+    const HEAP_SIZE: usize,
+    const MAX_BLOCKS: usize,
+    const OUTPUT_WIDTH: usize,
+> {
     tokens: Tokenized<MAX_TOKENS, MAX_LITERAL_CHARS>,
     token: usize,
     heap: CopyingHeap<u64, HEAP_SIZE, MAX_BLOCKS>,
-    stack: [StackFrame<MAX_LOCAL_VARS, MAX_LITERAL_CHARS>; STACK_DEPTH],
-    stack_level: usize,
+    stack: ProgramStack<MAX_LITERAL_CHARS, STACK_DEPTH, MAX_LOCAL_VARS>,
 }
 
-impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize, const STACK_DEPTH: usize, const MAX_LOCAL_VARS: usize, const HEAP_SIZE: usize, const MAX_BLOCKS: usize> Interpreter<MAX_TOKENS, MAX_LITERAL_CHARS, STACK_DEPTH, MAX_LOCAL_VARS, HEAP_SIZE, MAX_BLOCKS> {
+pub enum TickResult<T> {
+    Ok(T),
+    Err(TickError)
+}
+
+impl<T> TickResult<T> {
+    pub fn unwrap(self) -> T {
+        match self {
+            TickResult::Ok(v) => v,
+            TickResult::Err(e) => panic!("Interpreter Error: {e:?}")
+        }
+    }
+
+    pub fn map<U, F: FnOnce(T) -> U>(self, op: F) -> TickResult<U> {
+        match self {
+            TickResult::Ok(value) => TickResult::Ok(op(value)),
+            TickResult::Err(e) => TickResult::Err(e),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum TickError {
+    HeapIssue(gc_heap::HeapError),
+    ParseIssue(ParseError)
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum ParseError {
+    UnmatchedParen,
+    SyntaxError
+}
+
+impl<
+        const MAX_TOKENS: usize,
+        const MAX_LITERAL_CHARS: usize,
+        const STACK_DEPTH: usize,
+        const MAX_LOCAL_VARS: usize,
+        const HEAP_SIZE: usize,
+        const MAX_BLOCKS: usize,
+        const OUTPUT_WIDTH: usize,
+    >
+    Interpreter<MAX_TOKENS, MAX_LITERAL_CHARS, STACK_DEPTH, MAX_LOCAL_VARS, HEAP_SIZE, MAX_BLOCKS, OUTPUT_WIDTH>
+{
     pub fn new(program: &str) -> Self {
         let tokens = Tokenized::tokenize(program).unwrap();
         Self {
             tokens,
             token: 0,
             heap: CopyingHeap::new(),
-            stack: [StackFrame::default(); STACK_DEPTH],
-            stack_level: 0,
+            stack: ProgramStack::default(),
         }
     }
 
-    pub fn tick<I: InterpreterIo>(&mut self, io: &mut I) {
+    pub fn tick<I: InterpreterIo>(&mut self, io: &mut I) -> TickResult<()> {
         match self.tokens.tokens[self.token] {
             Token::Print => {
                 self.token += 1;
                 match self.tokens.tokens[self.token] {
                     Token::OpenParen => {
                         self.token += 1;
-
+                        let v = self.parse_expr();
+                        match self.print_value(&v, io) {
+                            TickResult::Ok(_) => {}
+                            TickResult::Err(e) => return TickResult::Err(e)
+                        }
+                        match self.tokens.tokens[self.token] {
+                            Token::CloseParen => {
+                                self.token += 1;
+                            }
+                            _ => return TickResult::Err(TickError::ParseIssue(ParseError::UnmatchedParen))
+                        }
                     }
-                    _ => {todo!{"Something or other"};}      
+                    _ => return TickResult::Err(TickError::ParseIssue(ParseError::SyntaxError))
                 }
             }
             Token::Symbol(s) => {
                 self.token += 1;
                 match self.tokens.tokens[self.token] {
-                    Token::Assign => {
-
-                    }
+                    Token::Assign => {}
                     Token::OpenParen => {
                         todo!("Function call");
                     }
-                    _ => {todo!{"Something or other"};}        
+                    _ => return TickResult::Err(TickError::ParseIssue(ParseError::SyntaxError))
                 }
             }
-            _ => {todo!{"Something or other"};}
+            _ => return TickResult::Err(TickError::ParseIssue(ParseError::SyntaxError))
         }
+        TickResult::Ok(())
     }
 
     fn parse_expr(&mut self) -> Value {
         match self.tokens.tokens[self.token] {
             Token::Number(n) => {
+                let (t, v) = if n.contains(&'.') {
+                    let float_val = make_float_from(&n);
+                    (ValueType::Integer, float_val.to_bits())
+                } else {
+                    (ValueType::Integer, make_int_from(&n))
+                };
+                let p = self.heap.malloc(1, &self.stack);
                 todo!("determine if int or float - then store")
             }
-            _ => {todo!{"Something or other"};}
+            _ => {
+                todo! {"Something or other"};
+            }
+        }
+    }
+
+    fn print_value<I: InterpreterIo>(&self, v: &Value, io: &mut I) -> TickResult<()> {
+        let mut output_buffer = [0; OUTPUT_WIDTH];
+        match v.output(&self.heap, &mut output_buffer) {
+            TickResult::Ok(num_words) => {
+                io.print(&output_buffer[0..num_words]);
+                TickResult::Ok(())
+            }
+            TickResult::Err(e) => TickResult::Err(e)
         }
     }
 }
@@ -98,17 +178,117 @@ struct Value {
     t: ValueType,
 }
 
+impl Value {
+    fn output<const HEAP_SIZE: usize, const MAX_BLOCKS: usize>(&self, heap: &CopyingHeap<u64, HEAP_SIZE, MAX_BLOCKS>, buffer: &mut [u8]) -> TickResult<usize> {
+        match self.t {
+            ValueType::Integer => {
+                match heap.load(self.location) {
+                    HeapResult::Ok(mut w) => {
+                        if w == 0 {
+                            buffer[0] = '0' as u8;
+                            return TickResult::Ok(1);
+                        }
+                        let mut start = if w >> 63 == 1 {
+                            buffer[0] = '-' as u8;
+                            w &= u64::MAX >> 1;
+                            1
+                        } else {
+                            0
+                        };
+                        let mut i = start;
+                        while w > 0 && i < buffer.len() {
+                            buffer[i] = (w % 10) as u8 + '0' as u8;
+                            w /= 10;
+                            i += 1;
+                        }
+                        while start < i {
+                            let temp = buffer[start];
+                            buffer[start] = buffer[i - start - 1];
+                            buffer[i - start - 1] = temp;
+                            start += 1;
+                        }
+
+                        TickResult::Ok(i)
+                    }
+                    HeapResult::Err(e) => TickResult::Err(TickError::HeapIssue(e))
+                }
+            }
+            ValueType::Float => todo!(),
+            ValueType::String => {
+                let mut p = self.location;
+                for i in 0..min(self.location.len(), buffer.len()) {
+                    match heap.load(p) {
+                        HeapResult::Ok(value) => {
+                            buffer[i] = value as u8;
+                        }
+                        HeapResult::Err(e) => return TickResult::Err(TickError::HeapIssue(e))
+                    }
+                }
+                TickResult::Ok(self.location.len())
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
 enum ValueType {
     Integer,
     Float,
     String,
 }
+    
+fn make_int_from(chars: &[char]) -> u64 {
+    let mut value = 0;
+    for c in chars.iter() {
+        value *= 10;
+        value += *c as u64 - '0' as u64;
+    }
+    value
+}
 
-impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize, const STACK_DEPTH: usize, const MAX_LOCAL_VARS: usize, const HEAP_SIZE: usize, const MAX_BLOCKS: usize> Tracer for Interpreter<MAX_TOKENS, MAX_LITERAL_CHARS, STACK_DEPTH, MAX_LOCAL_VARS, HEAP_SIZE, MAX_BLOCKS> {
+fn make_float_from(chars: &[char]) -> f64 {
+    let mut value = 0.0;
+    let mut shifter = None;
+    for c in chars.iter() {
+        if *c == '.' {
+            shifter = Some(0.1);
+        } else {
+            match shifter.as_mut() {
+                Some(shift) => {
+                    value += ((*c as u64 - '0' as u64) as f64) * *shift;
+                    *shift /= 10.0;
+                }
+                None => {
+                    value *= 10.0;
+                    value += (*c as u64 - '0' as u64) as f64;
+                }
+            }
+        }
+    }
+    value
+}
+
+#[derive(Copy, Clone)]
+struct ProgramStack<const MAX_LITERAL_CHARS: usize, const STACK_DEPTH: usize, const MAX_LOCAL_VARS: usize> {
+    stack: [StackFrame<MAX_LOCAL_VARS, MAX_LITERAL_CHARS>; STACK_DEPTH],
+    stack_level: usize,
+}
+
+impl<const MAX_LITERAL_CHARS: usize, const STACK_DEPTH: usize, const MAX_LOCAL_VARS: usize> ProgramStack<MAX_LOCAL_VARS, STACK_DEPTH, MAX_LITERAL_CHARS> {
+
+} 
+
+impl<const MAX_LITERAL_CHARS: usize, const STACK_DEPTH: usize, const MAX_LOCAL_VARS: usize> Default for ProgramStack<MAX_LOCAL_VARS, STACK_DEPTH, MAX_LITERAL_CHARS> {
+    fn default() -> Self {
+        Self { stack: [StackFrame::default(); STACK_DEPTH], stack_level: Default::default() }
+    }
+} 
+
+impl<const MAX_LITERAL_CHARS: usize, const STACK_DEPTH: usize, const MAX_LOCAL_VARS: usize> Tracer for ProgramStack<MAX_LOCAL_VARS, STACK_DEPTH, MAX_LITERAL_CHARS> {
     fn trace(&self, blocks_used: &mut [bool]) {
         todo!()
     }
-}
+} 
 
 #[derive(Copy, Clone, Default)]
 pub struct StackFrame<const MAX_LOCAL_VARS: usize, const MAX_LITERAL_CHARS: usize> {
@@ -122,7 +302,9 @@ pub enum TokenResult<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> {
     Err(TokenError),
 }
 
-impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> TokenResult<MAX_TOKENS, MAX_LITERAL_CHARS> {
+impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize>
+    TokenResult<MAX_TOKENS, MAX_LITERAL_CHARS>
+{
     pub fn unwrap(self) -> Tokenized<MAX_TOKENS, MAX_LITERAL_CHARS> {
         match self {
             TokenResult::Ok(t) => t,
@@ -174,7 +356,7 @@ impl<const MAX_LITERAL_CHARS: usize> Default for Token<MAX_LITERAL_CHARS> {
 }
 
 #[derive(Debug)]
-pub struct Tokenized<const MAX_TOKENS:usize, const MAX_LITERAL_CHARS: usize> {
+pub struct Tokenized<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> {
     tokens: [Token<MAX_LITERAL_CHARS>; MAX_TOKENS],
     num_tokens: usize,
 }
@@ -186,7 +368,7 @@ macro_rules! try_token {
                 $nc = 0;
                 Self::clear(&mut $sb);
             }
-            Some(e) => return TokenResult::Err(e)
+            Some(e) => return TokenResult::Err(e),
         }
     };
 }
@@ -199,7 +381,7 @@ macro_rules! try_add_char {
         } else {
             return TokenResult::Err(TokenError::LiteralTooLong);
         }
-    }
+    };
 }
 
 macro_rules! try_terminate {
@@ -219,12 +401,17 @@ macro_rules! try_terminate {
                 }
             }
         }
-    }
+    };
 }
 
-impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> Tokenized<MAX_TOKENS, MAX_LITERAL_CHARS> {
+impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize>
+    Tokenized<MAX_TOKENS, MAX_LITERAL_CHARS>
+{
     pub fn tokenize(text: &str) -> TokenResult<MAX_TOKENS, MAX_LITERAL_CHARS> {
-        let mut result = Self {num_tokens: 0, tokens: [Token::False; MAX_TOKENS]};
+        let mut result = Self {
+            num_tokens: 0,
+            tokens: [Token::False; MAX_TOKENS],
+        };
         let mut str_buffer = ['\0'; MAX_LITERAL_CHARS];
         let mut num_chars = 0;
         let mut inside_string = false;
@@ -241,7 +428,7 @@ impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> Tokenized<MAX_TOKE
                 try_add_char!(c, num_chars, str_buffer);
             } else if c.is_whitespace() {
                 try_terminate!(result, num_chars, str_buffer);
-            } 
+            }
         }
         TokenResult::Ok(result)
     }
@@ -275,14 +462,14 @@ impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> Tokenized<MAX_TOKE
             '}' => Some(Token::CloseCurly),
             '[' => Some(Token::OpenSquare),
             ']' => Some(Token::CloseSquare),
-            _ => None
+            _ => None,
         }
     }
 
     fn match_multiple(chars: &[char]) -> Option<Token<MAX_LITERAL_CHARS>> {
         match chars {
             [':', '='] => Some(Token::Assign),
-            ['=','='] => Some(Token::Equal),
+            ['=', '='] => Some(Token::Equal),
             ['t', 'r', 'u', 'e'] => Some(Token::True),
             ['f', 'a', 'l', 's', 'e'] => Some(Token::False),
             ['w', 'h', 'i', 'l', 'e'] => Some(Token::While),
@@ -293,20 +480,22 @@ impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> Tokenized<MAX_TOKE
             ['n', 'o', 't'] => Some(Token::Not),
             ['p', 'r', 'i', 'n', 't'] => Some(Token::Print),
             ['i', 'n', 'p', 'u', 't'] => Some(Token::Input),
-            _ => None
+            _ => None,
         }
     }
 
     fn is_number(chars: &[char]) -> bool {
-        chars[0].is_numeric() && chars.iter().filter(|c| **c == '.').count() <= 1 && chars.iter().all(|c| c.is_numeric() || *c == '.')
+        chars[0].is_numeric()
+            && chars.iter().filter(|c| **c == '.').count() <= 1
+            && chars.iter().all(|c| c.is_numeric() || *c == '.')
     }
 }
 
 #[cfg(test)]
 mod tests {
     use core::fmt::Display;
-    use std::fs::read_to_string;
     use std::collections::VecDeque;
+    use std::fs::read_to_string;
 
     use super::*;
 
@@ -332,18 +521,18 @@ mod tests {
         }
     }
 
-    impl <const MAX_LITERAL_CHARS: usize> Token<MAX_LITERAL_CHARS> {
+    impl<const MAX_LITERAL_CHARS: usize> Token<MAX_LITERAL_CHARS> {
         fn var(&self) -> Option<String> {
             match self {
                 Token::Symbol(chars) | Token::Number(chars) | Token::String(chars) => {
                     Some(chars.iter().take_while(|c| **c != '\0').collect())
                 }
-                _ => None
+                _ => None,
             }
         }
     }
 
-    impl <const MAX_LITERAL_CHARS: usize> Display for Token<MAX_LITERAL_CHARS> {
+    impl<const MAX_LITERAL_CHARS: usize> Display for Token<MAX_LITERAL_CHARS> {
         fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
             match self {
                 Token::Symbol(_) => {
@@ -355,12 +544,14 @@ mod tests {
                 Token::String(_) => {
                     write!(f, "String \"{}\"", self.var().unwrap())
                 }
-                token => write!(f, "{token:?}")
+                token => write!(f, "{token:?}"),
             }
         }
     }
 
-    impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> Display for Tokenized<MAX_TOKENS, MAX_LITERAL_CHARS> {
+    impl<const MAX_TOKENS: usize, const MAX_LITERAL_CHARS: usize> Display
+        for Tokenized<MAX_TOKENS, MAX_LITERAL_CHARS>
+    {
         fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
             for i in 0..self.num_tokens {
                 writeln!(f, "{}", self.tokens[i])?;
@@ -374,5 +565,26 @@ mod tests {
         let example = read_to_string("programs/average.prog").unwrap();
         let tokens = Tokenized::<500, 100>::tokenize(example.as_str()).unwrap();
         println!("{tokens}");
+    }
+
+    #[test]
+    fn test_parse_int() {
+        for s in [['1', '0'], ['1', '2'], ['2','1'], ['4', '3']] {
+            let expected = s.iter().collect::<String>().parse::<u64>().unwrap();
+            assert_eq!(expected, make_int_from(&s));
+        }
+    }
+
+    #[test]
+    fn test_parse_float() {
+        for s in [
+            ['1', '0', '.', '0', '0'], 
+            ['0', '1', '.', '0', '1'],
+            ['2', '5', '.', '1', '9'],
+            ['2', '.', '4', '4', '8']
+        ] {
+            let expected = s.iter().collect::<String>().parse::<f64>().unwrap();
+            assert_eq!(expected, make_float_from(&s));
+        }
     }
 }
