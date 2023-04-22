e0db5721946858018b1ddb3beace59f792634e60
gjf2a <ferrer@hendrix.edu>
2023-04-22 06:24:08 -0500

    more debug

--- src/lib.rs
+++ src/lib.rs
@@ -607,6 +607,7 @@ impl<
     }
 
     fn malloc_numeric_value(&mut self, value: u64, t: ValueType) -> TickResult<Value> {
+        println!("Storing {value} at {p:?}");
         match self.heap.malloc(1, &self.stack) {
             HeapResult::Ok(p) => match self.heap.store(p, value) {
                 HeapResult::Ok(_) => TickResult::Ok(Value { location: p, t }),
